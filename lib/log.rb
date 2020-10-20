b23ce1d7ed0ad74104532d2f3bbbaa8e9d13dd26
casualc0der <edwardsansome@gmail.com>
2020-10-20 20:17:35 +0100

    tidy up generate method

--- lib/log.rb
+++ lib/log.rb
@@ -16,14 +16,9 @@ class Log
   end
   def generate
     format.each do |logline|
-      x = data.find { |l| l.path == logline[0] }
-      if x.nil?
-        logger = LogLine.new(logline[0])
-        logger.add_ip(logline[1])
-        data << logger
-      else
-        x.add_ip(logline[1])
-      end
+      log= log_finder(logline)
+      add_new_logline(logline) if log.nil?
+      log.add_ip(logline[1]) unless log.nil?
     end
     @data
   end
@@ -36,5 +31,16 @@ class Log
   end
   def format_lines(line)
     line.split("\n").map {|line| line.split(" ")}
+  end
+  def log_finder(logline)
+    data.find { |l| l.path == logline[0] }
+  end
+  def add_new_logline(logline)
+    logger = LogLine.new(logline[0])
+    logger.add_ip(logline[1])
+    data << logger
+  end
+  def existing_logline(logline)
+    log_finder(logline)
   end
 end
