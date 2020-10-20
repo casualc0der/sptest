73325343ef8f89435e01d9037f0cfb2f73bca289
casualc0der <edwardsansome@gmail.com>
2020-10-20 21:18:43 +0100

    add guard for no file submission via CLI

--- lib/logger.rb
+++ lib/logger.rb
@@ -4,6 +4,7 @@ require_relative 'log'
 require_relative 'parser'
 class Logger
   def self.run
+    abort "USAGE: ruby bin/logger 'path/to/file'" if ARGV[0].nil?
     while true
       puts 'Return total (t) or unique (u) visits? please enter t or u:'
       log = Log.new(ARGV[0])
